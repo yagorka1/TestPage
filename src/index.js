4d72ffabf546dfa6139fff42be290194a876c2b8
Egor Grinevich <egor.grinevich1999@gmail.com>
2019-10-15 13:37:47 +0300

    add 'Footer'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './style.css';
+
+// Initialize and add the map
+// function initMap() {
+// The location of Uluru
+const uluru = { lat: 53.8920966, lng: 27.5482666 };
+// The map, centered at Uluru
+// eslint-disable-next-line no-undef
+const map = new window.google.maps.Map(
+  document.getElementById('map'), { zoom: 10, center: uluru }
+);
+  // The marker, positioned at Uluru
+//
+// eslint-disable-next-line no-undef
+const marker = new google.maps.Marker({ position: uluru, map });
+
+console.log(marker);
+alert('dd')
