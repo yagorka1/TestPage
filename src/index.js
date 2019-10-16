8ba61d16e29412d516aa08bd0f84a3eaedfd1611
Egor Grinevich <egor.grinevich1999@gmail.com>
2019-10-16 14:04:59 +0300

    add comment to photo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,59 @@
-jQuery('.lightzoom').lightzoom();
-      ( function( $ ) {
+// eslint-disable-next-line no-multi-assign
+const a = document.querySelector('.team_photo1').onclick = function () {
+  const b = document.querySelector('.team_message1');
+  b.classList.contains('hidd') ? b.classList.remove('hidd') : b.classList.add('hidd');
+  console.log(a);
+};
 
-$( '.lightzoom' ).lightzoom( {
-  speed:                 1000,   // скорость появления
-  imgPadding:            10,    // значение отступа у изображения
-  overlayOpacity:        '0.5', // прозрачность фона (от 0 до 1)
-  viewTitle:             true, // true, если надо показывать подпись к изобажению
-  isOverlayClickClosing: true, // true, если надо закрывать окно при клике по затемненной области
-  isWindowClickClosing:  true, // true, если надо закрывать окно при клике по любой области
-  isEscClosing:          true  // true, если надо закрывать окно при нажатии на кнопку Esc
-} );
+// eslint-disable-next-line no-multi-assign
+const c = document.querySelector('.team_photo2').onclick = function () {
+  const b = document.querySelector('.team_message2');
+  b.classList.contains('hidd') ? b.classList.remove('hidd') : b.classList.add('hidd');
+  console.log(a);
+};
+const d = document.querySelector('.team_photo3').onclick = function () {
+  const b = document.querySelector('.team_message3');
+  b.classList.contains('hidd') ? b.classList.remove('hidd') : b.classList.add('hidd');
+  console.log(a);
+};
 
-} )( jQuery );
+// eslint-disable-next-line no-multi-assign
+const e = document.querySelector('.team_photo4').onclick = function () {
+  const b = document.querySelector('.team_message4');
+  b.classList.contains('hidd') ? b.classList.remove('hidd') : b.classList.add('hidd');
+  console.log(a);
+};
+const f = document.querySelector('.team_photo5').onclick = function () {
+  const b = document.querySelector('.team_message5');
+  b.classList.contains('hidd') ? b.classList.remove('hidd') : b.classList.add('hidd');
+  console.log(a);
+};
+
+// eslint-disable-next-line no-multi-assign
+const g = document.querySelector('.team_photo6').onclick = function () {
+  const b = document.querySelector('.team_message6');
+  b.classList.contains('hidd') ? b.classList.remove('hidd') : b.classList.add('hidd');
+  console.log(a);
+};
+
+
+// /*eslint-disable*/
+// jQuery('.lightzoom').lightzoom();
+// // eslint-disable-next-line no-undef
+// (function ($) {
+//   // eslint-disable-next-line no-undef
+//   $( '.lightzoom' ).lightzoom({
+//     // eslint-disable-next-line no-undef
+//     speed: 1000,   // скорость появления
+//     imgPadding: 10,
+//     overlayOpacity: '0.5',
+//     viewTitle: true,
+//     isOverlayClickClosing: true,
+//     isWindowClickClosing: true,
+//     isEscClosing: true
+//       } );
+//     })( jQuery );
+
+// alert('fff')
+//     const a = document.getElementByClassName('team_photo2');
+//     console.log(a);
