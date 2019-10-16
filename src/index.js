706b9ca8800dd5252cccdfd663a9466df8668818
Egor Grinevich <egor.grinevich1999@gmail.com>
2019-10-16 04:23:08 +0300

    add increase images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+jQuery('.lightzoom').lightzoom();
+      ( function( $ ) {
 
+$( '.lightzoom' ).lightzoom( {
+  speed:                 1000,   // скорость появления
+  imgPadding:            10,    // значение отступа у изображения
+  overlayOpacity:        '0.5', // прозрачность фона (от 0 до 1)
+  viewTitle:             true, // true, если надо показывать подпись к изобажению
+  isOverlayClickClosing: true, // true, если надо закрывать окно при клике по затемненной области
+  isWindowClickClosing:  true, // true, если надо закрывать окно при клике по любой области
+  isEscClosing:          true  // true, если надо закрывать окно при нажатии на кнопку Esc
+} );
+
+} )( jQuery );
